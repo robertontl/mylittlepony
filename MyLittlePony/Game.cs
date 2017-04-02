0d57f2b57aacedae12bf5c77d30398876777608f
Roberto Natale <roberto.natale@phth.de>
2017-04-02 20:42:51 +0200

    [TASK] Adds correct functionality for every played round

--- MyLittlePony/Game.cs
+++ MyLittlePony/Game.cs
@@ -11,7 +11,6 @@ namespace MyLittlePony
         private List<Player> _players = new List<Player>();
         private List<Card> _cards = new List<Card>();
         private Player _lastWinner;
-        private int _winnerOfThisRound;
 
         public Game()
         {
@@ -33,9 +32,9 @@ namespace MyLittlePony
 
         public void createCards()
         {
-            int[] value = new int[32];
+            int[] value = new int[64];
             value[0] = 156;
-            value[1] = 2;
+            value[1] = 4;
             value[2] = 70;
             value[3] = 10000;
             value[4] = 165;
@@ -47,11 +46,11 @@ namespace MyLittlePony
             value[10] = 90;
             value[11] = 9000;
             value[12] = 123;
-            value[13] = 1;
+            value[13] = 4;
             value[14] = 95;
             value[15] = 7500;
             value[16] = 175;
-            value[17] = 2;
+            value[17] = 4;
             value[18] = 70;
             value[19] = 10000;
             value[20] = 163;
@@ -63,11 +62,43 @@ namespace MyLittlePony
             value[26] = 90;
             value[27] = 4000;
             value[28] = 196;
-            value[29] = 1;
+            value[29] = 4;
             value[30] = 95;
             value[31] = 2300;
+            value[32] = 156;
+            value[33] = 4;
+            value[34] = 70;
+            value[35] = 10000;
+            value[36] = 165;
+            value[37] = 4;
+            value[38] = 120;
+            value[39] = 15000;
+            value[40] = 193;
+            value[41] = 4;
+            value[42] = 90;
+            value[43] = 9000;
+            value[44] = 123;
+            value[45] = 4;
+            value[46] = 95;
+            value[47] = 7500;
+            value[48] = 175;
+            value[49] = 4;
+            value[50] = 70;
+            value[51] = 10000;
+            value[52] = 163;
+            value[53] = 4;
+            value[54] = 120;
+            value[55] = 15250;
+            value[56] = 193;
+            value[57] = 4;
+            value[58] = 90;
+            value[59] = 4000;
+            value[60] = 196;
+            value[61] = 4;
+            value[62] = 95;
+            value[63] = 2300;
 
-            string[] unit = new string[32];
+            string[] unit = new string[64];
             unit[0] = "cm";
             unit[1] = "int";
             unit[2] = "km/h";
@@ -100,8 +131,40 @@ namespace MyLittlePony
             unit[29] = "int";
             unit[30] = "km/h";
             unit[31] = "Euro";
+            unit[32] = "cm";
+            unit[33] = "int";
+            unit[34] = "km/h";
+            unit[35] = "Euro";
+            unit[36] = "cm";
+            unit[37] = "int";
+            unit[38] = "km/h";
+            unit[39] = "Euro";
+            unit[40] = "cm";
+            unit[41] = "int";
+            unit[42] = "km/h";
+            unit[43] = "Euro";
+            unit[44] = "cm";
+            unit[45] = "int";
+            unit[46] = "km/h";
+            unit[47] = "Euro";
+            unit[48] = "cm";
+            unit[49] = "int";
+            unit[50] = "km/h";
+            unit[51] = "Euro";
+            unit[52] = "cm";
+            unit[53] = "int";
+            unit[54] = "km/h";
+            unit[55] = "Euro";
+            unit[56] = "cm";
+            unit[57] = "int";
+            unit[58] = "km/h";
+            unit[59] = "Euro";
+            unit[60] = "cm";
+            unit[61] = "int";
+            unit[62] = "km/h";
+            unit[63] = "Euro";
 
-            bool[] higherValueWins = new bool[32];
+            bool[] higherValueWins = new bool[64];
             higherValueWins[0] = true;
             higherValueWins[1] = true;
             higherValueWins[2] = true;
@@ -134,8 +197,40 @@ namespace MyLittlePony
             higherValueWins[29] = true;
             higherValueWins[30] = true;
             higherValueWins[31] = true;
+            higherValueWins[32] = true;
+            higherValueWins[33] = true;
+            higherValueWins[34] = true;
+            higherValueWins[35] = true;
+            higherValueWins[36] = true;
+            higherValueWins[37] = true;
+            higherValueWins[38] = true;
+            higherValueWins[39] = true;
+            higherValueWins[40] = true;
+            higherValueWins[41] = true;
+            higherValueWins[42] = true;
+            higherValueWins[43] = true;
+            higherValueWins[44] = true;
+            higherValueWins[45] = true;
+            higherValueWins[46] = true;
+            higherValueWins[47] = true;
+            higherValueWins[48] = true;
+            higherValueWins[49] = true;
+            higherValueWins[50] = true;
+            higherValueWins[51] = true;
+            higherValueWins[52] = true;
+            higherValueWins[53] = true;
+            higherValueWins[54] = true;
+            higherValueWins[55] = true;
+            higherValueWins[56] = true;
+            higherValueWins[57] = true;
+            higherValueWins[58] = true;
+            higherValueWins[59] = true;
+            higherValueWins[60] = true;
+            higherValueWins[61] = true;
+            higherValueWins[62] = true;
+            higherValueWins[63] = true;
 
-            string[] name = new string[8];
+            string[] name = new string[16];
             name[0] = "Pferd 1";
             name[1] = "Pferd 2";
             name[2] = "Pferd 3";
@@ -144,16 +239,32 @@ namespace MyLittlePony
             name[5] = "Pferd 6";
             name[6] = "Pferd 7";
             name[7] = "Pferd 8";
+            name[8] = "Pferd 9";
+            name[9] = "Pferd 10";
+            name[10] = "Pferd 11";
+            name[11] = "Pferd 12";
+            name[12] = "Pferd 13";
+            name[13] = "Pferd 14";
+            name[14] = "Pferd 15";
+            name[15] = "Pferd 16";
 
-            string[] id = new string[8];
-            id[0] = "A2";
-            id[1] = "G4";
-            id[2] = "E6";
-            id[3] = "D5";
-            id[4] = "A5";
-            id[5] = "G7";
-            id[6] = "E1";
-            id[7] = "D3";
+            string[] id = new string[16];
+            id[0] = "A1";
+            id[1] = "B3";
+            id[2] = "C2";
+            id[3] = "D3";
+            id[4] = "E2";
+            id[5] = "F5";
+            id[6] = "G1";
+            id[7] = "H5";
+            id[8] = "A6";
+            id[9] = "B2";
+            id[10] = "C1";
+            id[11] = "D6";
+            id[12] = "E8";
+            id[13] = "F3";
+            id[14] = "G2";
+            id[15] = "H4";
 
             for (int i = 0; i <= name.Length-1; i++)
             {
@@ -219,11 +330,35 @@ namespace MyLittlePony
 
             Property choosenProperty = p.chooseProperty();
 
-            Console.WriteLine("Folgender Spieler hat die Runde gewonnen: " + this.compareCardsAndFindWinner(currentCards, choosenProperty).getName());
+            Player winner = this.compareCardsAndFindWinner(currentCards, choosenProperty);
+            this._lastWinner = winner;
+
+            Console.WriteLine("Folgender Spieler hat die Runde gewonnen: " + winner.getName());
         }
 
         public Player getCurrentPlayer()
         {
+            Player currentPlayer = this.getPlayers()[0];
+
+            if(_lastWinner != null)
+            {
+                currentPlayer = _lastWinner;
+            }
+
+            /*
+            if (this._lastWinner)
+            {
+                foreach(this._players in player)
+                {
+                    if(this._lastWinner == _)
+                    {
+                        currentPlayer = player;
+                    }
+                }
+            } 
+            */
+            
+            /*
             if (this._winnerOfThisRound == 0)
             { 
                 _lastWinner = this.getPlayers()[0];
@@ -240,8 +375,10 @@ namespace MyLittlePony
             {
                 _lastWinner = this.getPlayers()[3];
             }
-
-            return _lastWinner;
+            */
+            
+            return currentPlayer;
+            //return currentPlayer;
         }
 
         public List<Card> getCurrentCardsOfAllPlayers()
@@ -250,24 +387,21 @@ namespace MyLittlePony
 
             foreach (Player player in this.getPlayers())
             {
-                currentCardsOfAllPlayers.Add(player.getCurrentCard());
+                if (player.getCards().Count() != 0)
+                { 
+                    currentCardsOfAllPlayers.Add(player.getCurrentCard());
+                }
             }
 
             return currentCardsOfAllPlayers;
         }
 
         public Player compareCardsAndFindWinner(List<Card> currentCards, Property choosenProperty)
-        {   
-            int winnerOfThisRound = 99;
+        {
+            Player winnerOfThisRound = this._players[0];
 
-            string[] IDsIfValueIsEven = new string[4];
-
-            for (int i = 0; i <= 3; i++)
-            {
-                IDsIfValueIsEven[i] = currentCards[i].getID();
-            }
-
-            List<int> properties = new List<int>();
+            int bestValue = 0;
+            Card bestCard = currentCards[0];
 
             foreach (Card card in currentCards)
             {
@@ -275,65 +409,60 @@ namespace MyLittlePony
                 {
                     if (property.getUnit() == choosenProperty.getUnit())
                     {
-                        properties.Add(property.getValue());
+                        if (bestValue < property.getValue())
+                        {
+                            bestValue = property.getValue();
+                            bestCard = card;
+                        }                        
+                        else if(bestValue == property.getValue())
+                        {
+                            if (bestCard.getID()[1] < card.getID()[1])
+                            {
+                                bestCard = card;
+                            }
+                            else if(bestCard.getID()[1] == card.getID()[1])
+                            {
+                                if (bestCard.getID()[0] < card.getID()[0])
+                                { 
+                                    bestCard = card;
+                                }
+                            }
+                        }
                     }
                 }
             }
 
-            int bestValue = 0;
-            string IDofBestCard = "";
+            winnerOfThisRound = bestCard.getPlayer();
 
-            for (int i = 0; i <= _players.Count()-1; i++)
+            foreach (Card card in currentCards)
             {
-                if (bestValue == properties[i])
-                {
-                    Console.WriteLine("Identische Werte.");
-                    if (IDofBestCard[1] < currentCards[i].getID()[1])
-                    {
-                        winnerOfThisRound = i;
-                    }
-                    Console.WriteLine(IDofBestCard[1] + " " + currentCards[i].getID()[1]);
-                }
-
-                if (bestValue < properties[i])
-                {
-                    bestValue = properties[i];
-                    IDofBestCard = IDsIfValueIsEven[i];
-                    //IDofBestCard = currentCards[i].getID();
-                    winnerOfThisRound = i;
-                }
+                card.getPlayer().getCards().Remove(card);
+                card.setPlayer(winnerOfThisRound);
+                winnerOfThisRound.getCards().Add(card);
             }
 
-            Console.WriteLine("Größter Wert: " + bestValue + " " + choosenProperty.getUnit() + " KartenID: " + IDofBestCard);
+            Console.WriteLine("Größter Wert: " + bestValue + " " + choosenProperty.getUnit() + " KartenID: " + bestCard.getID());
 
-            for (int i = 0; i <= _players.Count()-1; i++)
-            {
-                //if (i != winnerOfThisRound)
-                //{
-
-                // Check ob gespielte Karte + gewonnene Karten von der Reihenfolge dem Gewinner hinten angehangen werden
-                    _players[winnerOfThisRound].getCards().Add(_players[i].getCards().First());
-                //}
-                _players[i].getCards().RemoveAt(0);
-            } 
-
-            this._winnerOfThisRound = winnerOfThisRound;
-
-            return this._players[winnerOfThisRound];
+            return winnerOfThisRound;
         }
         
         public bool hasEveryPlayerEnoughCards()
         {
             bool isTheGameStillOn = true;
 
+            List<Player> eliminatedPlayer = new List<Player>();
+
             for (int i = 0; i <= _players.Count()-1; i++)
             {
                 if (_players[i].countCards(_players[i]) == 0)
                 {
-                    _players.Remove(_players[i]);
-                    // darf erst auf false gesetzt werden, wenn nur noch 1 Spieler Karten hat
-                    isTheGameStillOn = false;
+                    eliminatedPlayer.Add(_players[i]);
                 }
+            }
+
+            if (eliminatedPlayer.Count() == 3)
+            {
+                isTheGameStillOn = false;
             }
 
             return isTheGameStillOn;
